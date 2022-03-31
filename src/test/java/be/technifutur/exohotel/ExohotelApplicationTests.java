f375bb585fa41de9648a425f5118bd0c58938108
Joachim <joachim.frances@hotmail.be>
2022-03-31 16:37:27 +0200

    Initial commit

---
+++ src/test/java/be/technifutur/exohotel/ExohotelApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.exohotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExohotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
